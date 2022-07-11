14d43c1fa10e4301ce8d8db840b7d0110799396e
byamini <baratamyamini222@gmail.com>
2022-07-11 11:28:34 +0530

    commit changes - repository created from eclipse

---
+++ src/test/java/LearningGithub/PracticingGit/Test1.java
@@ -0,0 +1,15 @@
+package LearningGithub.PracticingGit;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void method1()
+	{
+		System.out.println("from method1");
+	}
+	
+	
+
+}
